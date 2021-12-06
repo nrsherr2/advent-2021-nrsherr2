460ea7af08c77790673165c06cd0fd808ca93580
NSherrill <nsherrill@messerfinancial.com>
2021-12-06 16:26:30 -0500

    day 6 part 2

--- src/main/Day06.kt
+++ src/main/Day06.kt
@@ -3,15 +3,17 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val day6ExampleInput = readInput("Day06_Test")
     assertEquals(1, day6ExampleInput.size)
-    assertEquals(26, Day06.part1(day6ExampleInput.first(), 18))
-    assertEquals(5934, Day06.part1(day6ExampleInput.first(), 80))
-//    assertEquals(26984457539, Day06.part1(day6ExampleInput.first(), 256))
+    assertEquals(26L, Day06.part1(day6ExampleInput.first(), 18))
+    assertEquals(Day06.part1(day6ExampleInput.first(), 18),Day06.part2(day6ExampleInput.first(), 18))
+    assertEquals(5934L, Day06.part1(day6ExampleInput.first(), 80))
+    assertEquals(Day06.part1(day6ExampleInput.first(), 80),Day06.part2(day6ExampleInput.first(), 80))
+    assertEquals(26984457539, Day06.part2(day6ExampleInput.first(), 256))
     val day6Input = readInput("Day06_Input")
     assertEquals(1, day6Input.size)
     
     val timeToExecuteDay6 = measureTimeMillis {
         val part1Output = Day06.part1(day6Input.first(), 80)
-        val part2Output = 0
+        val part2Output = Day06.part2(day6Input.first(), 256)
         println(
             """
                 *** PART 1 ***
@@ -26,12 +28,36 @@ fun main() {
 }
 
 object Day06 {
-    fun part2(initialInput: String, numDays: Int): Int {
-        
-        TODO()
+    fun part2(initialInput: String, numDays: Int): Long {
+        val map = initialInput.split(",")
+            .groupingBy { -9L + it.toLong() }
+            .eachCount().mapValues { it.value.toLong() }.toMutableMap()
+        (-8 until numDays).forEach { dayNum ->
+            map[dayNum.toLong()]?.let { numSpawnedThatDay ->
+                val spawnSchedule = calculateSpawnSchedule(dayNum, numDays)
+                spawnSchedule.forEach { spawnDay ->
+                    val initialValue = map.getOrDefault(spawnDay.toLong(), 0L)
+                    map[spawnDay.toLong()] = initialValue + numSpawnedThatDay
+                }
+            }
+        }
+        return map.values.sum()
     }
     
-    fun part1(initialInput: String, numDays: Int): Int {
+    private fun calculateSpawnSchedule(daySpawned: Int, maxDay: Int): List<Int> {
+        val spawnDays = mutableListOf<Int>()
+        var currentNumber = 8
+        (daySpawned + 1 until maxDay).forEach { dayNum ->
+            currentNumber--
+            if (currentNumber == -1) {
+                currentNumber = 6
+                spawnDays.add(dayNum)
+            }
+        }
+        return spawnDays
+    }
+    
+    fun part1(initialInput: String, numDays: Int): Long {
         val fish = initialInput.split(",").map { it.toInt() }.toMutableList()
         (0 until numDays).forEach { day ->
             val fishSize = fish.size
@@ -42,9 +68,8 @@ object Day06 {
                     fish.add(8)
                 }
             }
-            println("Fish after $day days: ${fish.size}")
         }
-        return fish.size
+        return fish.size.toLong()
     }
     
 }
