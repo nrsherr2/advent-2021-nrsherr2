7009cee76cd785612038b30d6cbeadb4748c9c30
NSherrill <nsherrill@messerfinancial.com>
2021-12-08 08:30:03 -0500

    setup day 8

---
+++ src/main/Day08.kt
@@ -0,0 +1,30 @@
+import kotlin.math.abs
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val day8ExampleInput = readInput("Day08_Test")
+    val day8Input = readInput("Day08_Input")
+    
+    val timeToExecuteDay8 = measureTimeMillis {
+        val part1Output = 0
+        val part2Output = 0
+        println(
+            """
+                *** PART 1 ***
+                $part1Output
+                *** PART 2 ***
+                $part2Output
+                ***  END  ***
+            """.trimIndent()
+        )
+    }
+    println("Processing time: ${timeToExecuteDay8}ms")
+}
+
+object Day08 {
+
+}
+
+
+
+
