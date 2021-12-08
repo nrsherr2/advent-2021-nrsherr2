16eaf28114fdef48547abf595ba1b8938ca54137
NSherrill <nsherrill@messerfinancial.com>
2021-12-08 09:40:24 -0500

    working on day 8 part 2

--- src/main/Day08.kt
+++ src/main/Day08.kt
@@ -1,14 +1,14 @@
-import kotlin.math.abs
 import kotlin.system.measureTimeMillis
 
 fun main() {
     val day8ExampleInput = readInput("Day08_Test")
     assertEquals(26, Day08.part1(day8ExampleInput))
+//    assertEquals(61229, Day08.part2(day8ExampleInput))
     val day8Input = readInput("Day08_Input")
-    
+
     val timeToExecuteDay8 = measureTimeMillis {
         val part1Output = Day08.part1(day8Input)
-        val part2Output = 0
+        val part2Output = Day08.part2(day8Input)
         println(
             """
                 *** PART 1 ***
@@ -23,13 +23,39 @@ fun main() {
 }
 
 object Day08 {
-    fun part1(input:List<String>) = input.sumOf { line ->
+    fun part1(input: List<String>) = input.sumOf { line ->
         line.split(" | ")[1].split(" ").count { signal ->
-            signal.length in listOf(2,4,3,7)
+            signal.length in listOf(2, 4, 3, 7)
         }
     }
+
+    fun part2(input: List<String>) {
+
+    }
+
 }
 
+class SevenDigitDisplay(
+    var top: Char? = null,
+    var upRight: Char? = null,
+    var upLeft: Char? = null,
+    var mid: Char? = null,
+    var lwRight: Char? = null,
+    var lwLeft: Char? = null,
+    var bot: Char? = null,
+) {
+    val one
+        get() = displayString(upRight, lwRight)
+    val two
+    get() = displayString(top,upRight,mid,lwLeft,bot)
+    val three
+    get() = displayString(top,upRight,mid,lwRight,bot)
+    val four
+    get() = displayString(upLeft,upRight,mid,lwRight)
 
+    private fun displayString(vararg c: Char?) = c.joinToString("").toSortedString()
+}
+
+fun String.toSortedString() = toCharArray().sorted().joinToString("")
 
 
