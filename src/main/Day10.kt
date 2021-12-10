35923f92a8a5d33f60e642150e3ab7d0f3b11ae1
Nick Sherrill (nrsherr2) <nrsherr2@gmail.com>
2021-12-10 18:21:04 -0500

    day 10 part 2

--- src/main/Day10.kt
+++ src/main/Day10.kt
@@ -3,6 +3,7 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val day10ExampleInput = readInput("Day10_Test")
     assertEquals(26397, Day10.part1(day10ExampleInput))
+    assertEquals(288957L, Day10.part2(day10ExampleInput))
     val day10Input = readInput("Day10_Input")
 
     val timeToExecuteDay10 = measureTimeMillis {
@@ -35,30 +36,65 @@ object Day10 {
         }
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         val incompleteLines = input.filter { parseChunks(it) == null }
-        return 0
+        val scores = incompleteLines.map { ln ->
+            val openChunks = getChunks(ln).filter { it.close == null }.reversed()
+            var counter = 0L
+            openChunks.forEach { hunk ->
+                counter *= 5
+                counter += when (desiredChar(hunk.open)) {
+                    ')' -> 1L
+                    ']' -> 2L
+                    '}' -> 3L
+                    '>' -> 4L
+                    else -> throw IllegalArgumentException()
+                }
+            }
+            println("${openChunks.joinToString("") { desiredChar(it.open).toString() }}, $counter")
+            return@map counter
+        }
+        return median(scores)
     }
 
-    fun parseChunks(input: String): Char? {
+    private fun median(nums: List<Long>): Long {
+        return nums.sortedDescending().takeLast(nums.size / 2 + 1)[0]
+    }
+
+    private fun parseChunks(input: String): Char? {
         val chunks = mutableListOf<ChunkPair>()
         input.forEach { c ->
             if (c in listOf('(', '{', '[', '<')) {
                 chunks.add(ChunkPair(c, null))
             } else {
                 val lastOpenChunk = chunks.lastOrNull { it.close == null } ?: throw IllegalArgumentException()
-                val desiredChar = when (lastOpenChunk.open) {
-                    '(' -> ')'
-                    '{' -> '}'
-                    '<' -> '>'
-                    '[' -> ']'
-                    else -> throw IllegalArgumentException()
-                }
+                val desiredChar = desiredChar(lastOpenChunk.open)
                 if (c != desiredChar) return c
                 else lastOpenChunk.close = c
             }
         }
         return null
+    }
+
+    private fun getChunks(input: String): List<ChunkPair> {
+        val chunks = mutableListOf<ChunkPair>()
+        input.forEach { c ->
+            if (c in listOf('(', '{', '[', '<')) {
+                chunks.add(ChunkPair(c, null))
+            } else {
+                val lastOpenChunk = chunks.lastOrNull { it.close == null } ?: throw IllegalArgumentException()
+                lastOpenChunk.close = c
+            }
+        }
+        return chunks
+    }
+
+    private fun desiredChar(c: Char): Char = when (c) {
+        '(' -> ')'
+        '{' -> '}'
+        '<' -> '>'
+        '[' -> ']'
+        else -> throw IllegalArgumentException()
     }
 
     data class ChunkPair(var open: Char, var close: Char?)
