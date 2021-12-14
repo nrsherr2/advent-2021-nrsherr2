ad2c2f6b584faef0922d37ea1a7840bc464e6802
NSherrill <nsherrill@messerfinancial.com>
2021-12-14 13:50:54 -0500

    day 14 part 2, but inefficient

--- src/main/Day14.kt
+++ src/main/Day14.kt
@@ -3,6 +3,8 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val day14ExampleInput = readInput("Day14_Test")
     assertEquals(1588, Day14.part1(day14ExampleInput))
+    assertEquals(5, Day14.part2(day14ExampleInput, 2))
+    assertEquals(1588, Day14.part2(day14ExampleInput, 10))
 //    assertEquals(1588, Day14.part2(day14ExampleInput))
     assertEquals(2188189693529, Day14.part2(day14ExampleInput))
     val day14Input = readInput("Day14_Input")
@@ -34,25 +36,78 @@ object Day14 {
         return counts.maxOf { it.value } - counts.minOf { it.value }
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>, maxTime: Int = 40): Int {
         val list = input.first()
         val rules = input.subList(2, input.size).associate {
             val (a, b) = it.split(" -> ")
             a to b
         }
         val results = mutableListOf<BloomResult>()
-        list.windowed(2, 1).forEach {window->
-            val updatedRules = rules.toMutableMap()
-                .apply { putAll(results.associate { res -> res.condition to results.indexOf(res).toString() }) }
-            val bloomAfter20 = bloom(window,20,updatedRules)
-            println(bloomAfter20)
-            val map = calculateBloom(window, updatedRules, 40)
+        val michaelsoft = list.windowed(2, 1)
+        michaelsoft.forEachIndexed { i, window ->
+//            val updatedRules = rules.toMutableMap()
+//                .apply { putAll(results.associate { res -> res.condition to results.indexOf(res).toString() }) }
+
+            val b20 = bloom(window, maxTime / 2, rules)
+            val binbows = b20.windowed(2, 1)
+//
+//            val bin = binbows.let {
+//                val last = it.last()
+//                val sl = it.subList(0, it.size - 1)
+//                sl.distinct().associateWith { a -> sl.count {s-> s == a } }.plus(last to 1)
+//            }
+//            val bin20 = bin.entries.mapIndexed { j, (subWind, mult) ->
+//                calculateBloom(subWind, rules, maxTime / 2).toMutableMap().apply {
+//                    if (j < binbows.size - 1) this[subWind.last().toString()] = this[subWind.last().toString()]!! - 1
+//                    this.keys.forEach { this[it] = this[it]!! * mult }
+//                }
+//            }
+//            val m20 = bin20.foldRight(mutableMapOf<String, Int>()) { src, dest ->
+//                src.entries.forEach { ent ->
+//                    if (dest.containsKey(ent.key)) {
+//                        dest[ent.key] = dest[ent.key]!!.plus(ent.value)
+//                    } else {
+//                        dest[ent.key] = ent.value
+//                    }
+//                }
+//                dest
+//            }.apply {
+//                if (i < michaelsoft.size - 1) this[window.last().toString()] = this[window.last().toString()]!! - 1
+//            }
+
+            val bloomsFrom20 = binbows.mapIndexed { j, subWind ->
+                println("$i, $j")
+                calculateBloom(subWind, rules, maxTime / 2).toMutableMap().apply {
+                    if (j < binbows.size - 1) this[subWind.last().toString()] = this[subWind.last().toString()]!! - 1
+                }
+            }
+            val map = bloomsFrom20.foldRight(mutableMapOf<String, Int>()) { src, dest ->
+                src.entries.forEach { ent ->
+                    if (dest.containsKey(ent.key)) {
+                        dest[ent.key] = dest[ent.key]!!.plus(ent.value)
+                    } else {
+                        dest[ent.key] = ent.value
+                    }
+                }
+                dest
+            }.apply {
+                if (i < michaelsoft.size - 1) this[window.last().toString()] = this[window.last().toString()]!! - 1
+            }
+//            println(m20)
+            println(map)
             results.add(BloomResult(window, map))
         }
-        println(results)
-        TODO()
-//        val counts = calculateBloom(list, rules, 40)
-//        return counts.maxOf { it.value } - counts.minOf { it.value }
+        val counts = results.map { it.outputMap }.foldRight(mutableMapOf<String, Int>()) { src, dest ->
+            src.entries.forEach { ent ->
+                if (dest.containsKey(ent.key)) {
+                    dest[ent.key] = dest[ent.key]!!.plus(ent.value)
+                } else {
+                    dest[ent.key] = ent.value
+                }
+            }
+            dest
+        }
+        return counts.maxOf { it.value } - counts.minOf { it.value }
     }
 
     private fun calculateBloom(
@@ -69,7 +124,6 @@ object Day14 {
     ): String {
         var lt = list
         for (i in 0 until bloomSize) {
-            println(i)
             lt = lt.asSequence().mapIndexed { index, thisChar ->
                 if (index + 1 == lt.length) thisChar.toString()
                 else {
