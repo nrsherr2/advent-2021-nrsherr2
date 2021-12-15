3401d765a25b36f108e56bc526f9f1413e1821a2
NSherrill <nsherrill@messerfinancial.com>
2021-12-15 11:47:34 -0500

    faulty day 15 part 1

--- src/main/Day15.kt
+++ src/main/Day15.kt
@@ -23,13 +23,62 @@ fun main() {
 
 object Day15 {
     fun part1(input: List<String>): Int {
-        return 0
+        val grid = parseInput(input)
+        val maxStackSize = grid.flatten().size / 2
+        val end = grid[grid.lastIndex][grid[0].lastIndex]
+        return recursiveSearch(Int.MAX_VALUE, listOf(grid[0][0]), end, 0, maxStackSize)
     }
 
     fun part2(input: List<String>): Int {
         return 0
     }
 
+    private fun recursiveSearch(
+        currentMinCost: Int,
+        currentChain: List<Node>,
+        end: Node,
+        currentLevel: Int,
+        maxLevel: Int
+    ): Int {
+        if (currentLevel == maxLevel) return Int.MAX_VALUE.also { println("too far!") }
+        var mutableMinCost = currentMinCost
+        val currentNode = currentChain.last()
+        currentNode.neighbors.forEach { neighborNode ->
+            val neighborCost = currentChain.chainCost() + neighborNode.costToEnter
+            if (neighborNode == end) return neighborCost.also {
+                println(
+                    "${
+                        currentChain.plus(neighborNode).map { it.costToEnter }
+                    } $neighborCost"
+                )
+            }
+            if (neighborCost > mutableMinCost || currentChain.contains(neighborNode)) return@forEach
+            val curseDown =
+                recursiveSearch(mutableMinCost, currentChain + neighborNode, end, currentLevel + 1, maxLevel)
+            if (curseDown < mutableMinCost) mutableMinCost = curseDown
+        }
+        return mutableMinCost
+    }
+
+    private fun parseInput(input: List<String>): List<List<Node>> {
+        val grid = input.map { it.map { num -> Node(num.digitToInt()) } }
+        grid.forEachIndexed { rowNum, nodes ->
+            nodes.forEachIndexed { colNum, node ->
+                node.neighbors =
+                    listOf(rowNum - 1 to colNum, rowNum + 1 to colNum, rowNum to colNum - 1, rowNum to colNum + 1)
+                        .filter { it.first in grid.indices && it.second in nodes.indices }
+                        .map { grid[it.first][it.second] }
+                        .toSet()
+            }
+        }
+        return grid
+    }
+
+    private class Node(val costToEnter: Int) {
+        var neighbors: Set<Node> = setOf()
+    }
+
+    private fun List<Node>.chainCost() = if (this.size <= 1) 0 else this.subList(1, size).sumOf { it.costToEnter }
 }
 
 
