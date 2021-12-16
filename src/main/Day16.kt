71e2091ac929fbfd74cb742f0ba68b6bc3523a72
NSherrill <nsherrill@messerfinancial.com>
2021-12-16 09:57:06 -0500

    I'll come back to this later

--- src/main/Day16.kt
+++ src/main/Day16.kt
@@ -5,8 +5,6 @@ fun main() {
     Day16.examplesPart1.forEach {
         assertEquals(it.part1Expected, Day16.part1(it.input))
     }
-//    assertEquals(40, Day16.part1(day16ExampleInput))
-//    assertEquals(316, Day16.part2(day16ExampleInput))
     val day16Input = readInput("Day16_Input")
 
     val timeToExecuteDay16 = measureTimeMillis {
@@ -27,6 +25,8 @@ fun main() {
 
 object Day16 {
     fun part1(input: String): Int {
+        val rawBits = input.toCharArray().joinToString("") { it.digitToInt(16).toString(2).padStart(4, '0') }
+        println(rawBits)
         return 0
     }
 
@@ -34,7 +34,24 @@ object Day16 {
         return 0
     }
 
+    fun parsePackets(input: String, pointer: Int, desiredNumPackets: Int? = null): Pair<List<Packet>, Int> {
+        var pt = pointer
+        val parsedPackets = mutableListOf<Packet>()
+        while (pt < input.length && (desiredNumPackets == null || parsedPackets.size < desiredNumPackets)) {
+            val version = input.substring(pt, pt + 3).also { pt += 3 }.toInt(2)
+            val type = input.substring(pt, pt + 3).also { pt += 3 }.toInt(2)
+        }
+        return parsedPackets to pt
+    }
+    fun parseLiteralPacket(input: String,pointer: Int,version: Int,type: Int):Pair<Packet, Int>{
+        var pt = pointer
+        var rep = true
+        var parsedCharacterCount = 0
+        TODO()
+    }
+
     val examplesPart1 = listOf(
+        ExampleToExpected("D2FE28", 6),
         ExampleToExpected("8A004A801A8002F478", 16),
         ExampleToExpected("620080001611562C8802118E34", 12),
         ExampleToExpected("C0015000016115A2E0802F182340", 23),
@@ -43,7 +60,7 @@ object Day16 {
 
     open class Packet(var version: Int, var type: Int)
     class LiteralPacket(version: Int, type: Int, var value: Int) : Packet(version, type)
-    class OperatorPacket(version: Int, type: Int, var indicator: Boolean) {
+    class OperatorPacket(version: Int, type: Int, var indicator: Boolean) : Packet(version, type) {
         var subPackets: MutableList<Packet> = mutableListOf()
     }
 
